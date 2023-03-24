4896983e923cd7c76f5c09da59c6d941aa2d169a
Replit user <>
2023-03-24 17:59:04 +0000

    new try

--- list.c
+++ list.c
@@ -58,7 +58,7 @@ void * nextList(List * list) {
     return NULL;
   }
   
-  list->current = list->current->next;
+  list->current->data = list->current->next;
     return list->current->next->data;
 }
 
